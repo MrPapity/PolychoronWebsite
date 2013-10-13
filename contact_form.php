f515166d7b0e3871dcdb3e66c6eda940e5e6d18e
cristiankrieger <crstn.krieger@gmail.com>
2013-10-13 18:36:24 -0500

    Fixes

--- contact_form.php
+++ contact_form.php
@@ -7,30 +7,24 @@ $visitor_email = '';
 $visitor_subject = '';
 $user_message = '';
 
-if(isset($_POST['submit']))
-{
-	
+if(isset($_POST['submit'])){
 	$name = $_POST['name'];
 	$visitor_email = $_POST['email'];
 	$user_message = $_POST['message'];
 	$visitor_subject = $_POST['subject'];
 
-	if(empty($name)||empty($visitor_email)||empty($visitor_subject))
-	{
+	if(empty($name)||empty($visitor_email)||empty($visitor_subject)){
 		$errors .= "\n Todos los campos son requeridos. ";	
 	}
-	if(IsInjected($visitor_email))
-	{
-		$errors .= "\n Bad email value!";
+	if(IsInjected($visitor_email)){
+		$errors .= "\n Correo inválido.";
 	}
 	if(empty($_SESSION['6_letters_code'] ) ||
-	  strcasecmp($_SESSION['6_letters_code'], $_POST['6_letters_code']) != 0)
-	{
-		$errors .= "\n The captcha code does not match!";
+	  strcasecmp($_SESSION['6_letters_code'], $_POST['6_letters_code']) != 0){
+		$errors .= "\n Captcha incorrecto.";
 	}
 	
-	if(empty($errors))
-	{
+	if(empty($errors)){
 		$to = $email;
 		$subject="New Form Submission: $visitor_subject";
 		$from = $email;
@@ -41,14 +35,21 @@ if(isset($_POST['submit']))
 		$headers = "From: $from \r\n";
 		$headers .= "Reply-To: $visitor_email \r\n";
 		
-		mail($to, $subject, $body,$headers);
-		
-		header('Location: thankyou.html');
+		if(mail($to, $subject, $body,$headers)){
+			print '<script type="text/javascript">'; 
+			print 'alert("Correo enviado correctamente.")'; 
+			print '</script>';
+		}else{
+			print '<script type="text/javascript">'; 
+			print 'alert("Error: Correo no pudo ser enviado.")'; 
+			print '</script>';
+		}
+
+		//header('Location: thankyou.html');
 	}
 }
 
-function IsInjected($str)
-{
+function IsInjected($str){
   $injections = array('(\n+)',
               '(\r+)',
               '(\t+)',
@@ -59,12 +60,10 @@ function IsInjected($str)
               );
   $inject = join('|', $injections);
   $inject = "/$inject/i";
-  if(preg_match($inject,$str))
-    {
+  if(preg_match($inject,$str)){
     return true;
   }
-  else
-    {
+  else{
     return false;
   }
 }
@@ -78,72 +77,54 @@ function IsInjected($str)
         <link rel="stylesheet" type="text/css" href="CSS/main_style.css" />
         <title>Polychoron</title>
         <script language="JavaScript" src="scripts/gen_validatorv31.js" type="text/javascript"></script>
-      	<a href="INDEX.HTML">
-	    <img src="https://dl-web.dropbox.com/get/Krieger%20Electronics/Website/Polychoron_notext.png?w=AACem8gdCpLevOe1rCP10NHdHbQaw2g_q6rXjSNscJCTEA"/>
-	    </a>
-	    <div class="BackMenu"> </div>
-	    <div class="TopBar">
-	    	<table>
-	    		<br>
-	    		<tr> </tr>
-	    		<tr> <a class="link" href="AboutUs.html">Acerca de</a></tr>
-	    		<tr> </tr> <tr> | </tr> <tr> </tr>
-	    		<tr> <a class="link" href="SOMETHING">Servicios</a></tr>
-	    		<tr> </tr> <tr> | </tr> <tr> </tr>
-	    		<tr> <a class="link" href="contact_form.php">Contacto</a></tr>
-	    		<tr> </tr> <tr> | </tr> <tr> </tr>
-	    		<tr> <a class="link" href="SignIn.html">Ingreso</a></tr>
-	    	</table>
-	    </div> 
     </head>
-    <body>	    
-    	<div id="container">				        
-	        <table class="Content">
-				<tr>
-					<td class="ContactForm" rowspan="2" width="60%" height="600px">
-						<?php
-							if(!empty($errors)){
-								echo "<p class='err'>".nl2br($errors)."</p>";
-							}
-						?>
-						<div id='contact_form_errorloc' class='err'></div>
-						<form method="post" action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>">
-						Nombre:<br>
-						<input type="text" name="name" value='<?php echo htmlentities($name) ?>'><br>
-						<br>
-						Correo Electrónico:<br>
-						<input type="text" name="email" value='<?php echo htmlentities($visitor_email) ?>'><br>
-						<br>
-						Asunto:<br>
-						<input type="text" name="subject" value='<?php echo htmlentities($visitor_subject) ?>'><br>
-						<br>
-						Mensaje:<br>
-						<textarea name="message" rows=8 cols=30><?php echo htmlentities($user_message) ?></textarea><br>
-						<br>
-						<img src="captcha_code_file.php?rand=<?php echo rand(); ?>" id='captchaimg' ><br>
-						<br>
-						Por favor, introduzca el Captcha mostrado arriba:<br>
-						<input id="6_letters_code" name="6_letters_code" type="text"><br>
-						<small>¿No ve la imagen? Haga click <a href='javascript: refreshCaptcha();'>aquí</a>.</small><br>
-						<input type="submit" value="Enviar" name='submit'>
-						<input type="reset" value="Borrar">
-						</form>
-						<script language="JavaScript">
-							var frmvalidator  = new Validator("contact_form");
-							frmvalidator.EnableOnPageErrorDisplaySingleBox();
-							frmvalidator.EnableMsgsTogether();
-							frmvalidator.addValidation("name","req","Por favor, proporcione su nombre"); 
-							frmvalidator.addValidation("email","req","Por favor, proporcione su email"); 
-							frmvalidator.addValidation("email","email","Por favor, proporcione un email válido"); 
-						</script>
-						<script language='JavaScript' type='text/javascript'>
-							function refreshCaptcha(){
-								var img = document.images['captchaimg'];
-								img.src = img.src.substring(0,img.src.lastIndexOf("?"))+"?rand="+Math.random()*1000;
-							}
-						</script>
-					</td>									
-			</table>
+    <body>
+    	<a href="INDEX.HTML">
+	    <img src="Polychoron.png"/>
+	    </a>
+	    <div class="AboutUs"><a href="AboutUs.html"><p class="font">Acerca de</p></a></div>
+	    <div class="Services"><a href="http:www.hulur.com"><p class="font">Servicios</p></a></div>
+	    <div class="Contact"><a href="contact_form.php"><p class="font">Contacto</p></a></div>
+	    <div class="SingIn"><a href ="SignIn.html"><p class="font">Ingreso</p></a></div>
+    	<div id="container">	
+    		<form method="post" action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>">			        
+		        <table>
+					<tr>
+						<td>
+							<div id='contact_form_errorloc' class='err'>
+								<?php
+									if(!empty($errors)){
+										echo "<p class='err'>".nl2br($errors)."</p>";
+									}
+								?>
+							</div>
+						</td>
+					<tr>
+						<td>Nombre:</td>
+						<td><input type="text" name="name" value='<?php echo htmlentities($name) ?>'></td>
+					</tr>
+					<tr>
+						<td>Correo Electrónico:</td>
+						<td><input type="text" name="email" value='<?php echo htmlentities($visitor_email) ?>'></td>
+					</tr>
+					<tr>
+						<td>Asunto:</td>
+						<td><input type="text" name="subject" value='<?php echo htmlentities($visitor_subject) ?>'></td>
+					</tr>
+					<tr>
+						<td>Mensaje:</td>
+						<td><textarea name="message" rows=8 cols=30><?php echo htmlentities($user_message) ?></textarea></td>
+					</tr>
+					<tr>
+						<td><img class="Captcha" src="captcha_code_file.php?rand=<?php echo rand(); ?>" id='captchaimg'></td>
+						<td>Por favor, introduzca el Captcha mostrado:<br><input id="6_letters_code" name="6_letters_code" type="text"><br>
+							<small>¿No ve la imagen? Haga click <a href='javascript: refreshCaptcha();'>aquí</a>.</small><br>
+							<input type="submit" value="Enviar" name='submit'>
+							<input type="reset" value="Borrar">
+						</td>
+					</tr>
+				</table>
+			</form>
 			<table class="Footer">
 				<tr>
 					<td width="25%">Mapa del Sitio</td>
@@ -155,3 +136,17 @@ function IsInjected($str)
 		</div>
     </body>
 </html>
+<script language="JavaScript">
+	var frmvalidator  = new Validator("contact_form");
+	frmvalidator.EnableOnPageErrorDisplaySingleBox();
+	frmvalidator.EnableMsgsTogether();
+	frmvalidator.addValidation("name","req","Por favor, proporcione su nombre"); 
+	frmvalidator.addValidation("email","req","Por favor, proporcione su email"); 
+	frmvalidator.addValidation("email","email","Por favor, proporcione un email válido"); 
+</script>
+<script language='JavaScript' type='text/javascript'>
+	function refreshCaptcha(){
+		var img = document.images['captchaimg'];
+		img.src = img.src.substring(0,img.src.lastIndexOf("?"))+"?rand="+Math.random()*1000;
+	}
+</script>
